4661dc0f9beafaa93f41ae4777a6a630d90b7ba6
Daria <dashavolodina@gmail.com>
2023-02-03 09:27:23 +0800

    довален цикл нахождения 3ей цифры в любом заданном числе

--- sem2_002/Program.cs
+++ sem2_002/Program.cs
@@ -6,19 +6,19 @@
 
 Console.WriteLine("Введите целое число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number < 100) Console.WriteLine("Третьей цифры нет");
-if (number > 99)
+if (number / 100 == 0)
 {
-    if (number > 99 & number < 1000) number = number % 10;
-    if (number > 999 & number < 10000) number = number % 100 / 10;
-    if (number > 9999 & number < 100000) number = number % 1000 / 100;
-    if (number > 99999 & number < 1000000) number = number % 10000 / 1000;
-    if (number > 999999 & number < 10000000) number = number % 100000 / 10000;
-    if (number > 9999999 & number < 100000000) number = number % 1000000 / 100000;
-    if (number > 99999999 & number < 1000000000) number = number % 10000000 / 1000000;
-    Console.WriteLine($"Третья цифра в этом числе > {number}");
+    Console.WriteLine("Третьей цифры нет");
 }
+else
+{
+    while (number > 1000)
+    {
+        number = number / 10;
+    }
+    Console.WriteLine($"Третья цифра в этом числе > {number % 10}");
 
+}
 
 
 
